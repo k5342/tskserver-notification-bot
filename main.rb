7ec8762a72f86cf5b62b8ab0e641f553460911fa
k <ksswre@yahoo.co.jp>
2021-09-19 01:28:06 +0900

    restruct main loop

--- main.rb
+++ main.rb
@@ -24,15 +24,22 @@ require_relative './notifymanager'
   access_token_secret: ENV['TWITTER_OAUTH_TOKEN_SECRET'],
 ))
 
+def fetch_status(client, url)
+  json = client.get(url).body
+  return JSON.parse(json, symbolize_names: true)
+end
+
 @servers = {}
+@status_before = {}
+@onlines_before = {}
 @client = HTTPClient.new
 loop do
   begin
-    json = @client.get(ENV['TSKSERVER_API_URL']).body
-    data = JSON.parse(json, symbolize_names: true)
-    
+    data = fetch_status(@client, ENV['TSKSERVER_API_URL'])
     data.each do |k, v|
       name, body, status, last_checked_at = k, v[:body], v[:status], v[:last_checked_at]
+      
+      events = []
       
       # NOTE: This is just a workaround due to an insufficient API structure
       @servers[name] = ServerInfo.new(
@@ -41,64 +48,54 @@ loop do
         website_url: 'https://mc.ksswre.net',
         infos: body,
       )
-      begin
-        case status
-        when 'online'
-          if @status_before
-            if @status_before != status
-              event = Events::ServerUp.new(
-                server: @servers[name],
-                last_checked_at: last_checked_at,
-              )
-              @notify_manager.fire_event(event)
-            end
-          end
+      if status == 'online'
+        if @status_before[name] && @status_before[name] != status
+          events << Events::ServerUp.new(
+            server: @servers[name],
+            last_checked_at: last_checked_at,
+          )
+        end
+        
+        onlines = body[:players][:sample] || []
+        if @onlines_before[name]
+          onlines_diff = onlines - @onlines_before[name]
           
-          @onlines = body[:players][:sample] || []
-          if @onlines_before
-            onlines_diff = @onlines - @onlines_before
-            
-            if onlines_diff.size > 0
-              event = Events::UserLogined.new(
-                server: @servers[name],
-                last_checked_at: last_checked_at,
-                onlines_diff: onlines_diff,
-                onlines: @onlines,
-              )
-              @notify_manager.fire_event(event)
-            end
-          end
-          @onlines_before = @onlines
-        else
-          if @status_before
-            if @status_before != status
-              # TODO: API may include error details in body, remove them after API changes
-              event = Events::ServerDown.new(
-                server: @servers[name],
-                last_checked_at: last_checked_at,
-                infos: {
-                  error: body[:error],
-                  detail: body[:detail],
-                }
-              )
-              @notify_manager.fire_event(event)
-            end
+          if onlines_diff.size > 0
+            events << Events::UserLogined.new(
+              server: @servers[name],
+              last_checked_at: last_checked_at,
+              onlines_diff: onlines_diff,
+              onlines: onlines,
+            )
           end
         end
-      rescue => e
-        pp e
-        puts e.backtrace
-      ensure
-        @status_before = status
+        @onlines_before[name] = onlines
+      else
+        if @status_before[name] && @status_before[name] != status
+          # TODO: API may include error details in body, remove them after API changes
+          events << Events::ServerDown.new(
+            server: @servers[name],
+            last_checked_at: last_checked_at,
+            infos: {
+              error: body[:error],
+              detail: body[:detail],
+            }
+          )
+        end
       end
       
-      @notify_manager.heartbeat()
+      events.each do |event|
+        @notify_manager.fire_event(event)
+      end
+      
+      @status_before[name] = status
     end
   rescue => e
     pp e
     puts e.backtrace
+  ensure
+    @notify_manager.heartbeat()
+    sleep 10
   end
-  
-  sleep 10
 end
 
